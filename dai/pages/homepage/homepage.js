7d61f2ba052592b36948f4cc8feb13079101076a
blacksheep107 <blacksheep107@126.com>
2020-11-10 20:47:58 +0800

    2

---
+++ dai/pages/homepage/homepage.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    catelist:[
+      {
+        id:1,
+        image_src: '/photo/tag1.png'
+      },
+      {
+        id:2,
+        image_src: '/photo/tag2.png'
+      },
+      {
+        id:3,
+        image_src: '/photo/tag3.png'
+      }
+    ]
+  },
+})
---
+++ dai/pages/mine/mine.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    catelist:[
+      {
+        id:1,
+        image_src: '/photo/w1.png'
+      },
+      {
+        id:2,
+        image_src: '/photo/w2.png'
+      },
+      {
+        id:3,
+        image_src: '/photo/w3.png'
+      }
+    ]
+  },
+})
